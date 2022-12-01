acb5ed540e5143710ddf34eb7ff3afeca5f6469f
Cyprian Adamski <cyprian.adamski@gmail.com>
2022-12-01 16:51:43 +0100

    error in super in AmountWidget

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,70 +1,59 @@
 import { settings, select } from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-class AmountWidget {
+class AmountWidget extends BaseWidget{
+  super(element, settings.amountWidget.defaultValue);
+
   constructor(element) {
     const thisWidget = this;
-
-    console.log('amountWidget:', thisWidget);
-    console.log('constrctor argument:', element);
-
-    thisWidget.value = settings.amountWidget.defaultValue;
-
     thisWidget.getElements(element);
-    thisWidget.setValue(thisWidget.input.value);
+    
     thisWidget.initActions();
+    // console.log('amountWidget:', thisWidget);
+    // console.log('constrctor argument:', element);
   }
   getElements(element) {
     const thisWidget = this;
 
-    thisWidget.element = element; // element jest referencją do do tego samego elementu DOM, co thisProduct.amountWidgetElem. ?jak????????
-    thisWidget.input = thisWidget.element.querySelector(
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(
       select.widgets.amount.input
     );
-    thisWidget.linkDecrease = thisWidget.element.querySelector(
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(
       select.widgets.amount.linkDecrease
     );
-    thisWidget.linkIncrease = thisWidget.element.querySelector(
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(
       select.widgets.amount.linkIncrease
     );
   }
-  setValue(value) {
-    const thisWidget = this;
-    const newValue = parseInt(value);
+  
 
-    /* TODO Add validation */
-    if (
-      thisWidget.value !== newValue &&
-      !isNaN(newValue) &&
-      newValue >= settings.amountWidget.defaultMin &&
-      newValue <= settings.amountWidget.defaultMax
-    ) {
-      thisWidget.value = newValue;
-      this.annonce();
-    }
-
-    //thisWidget.value = newValue;
-    thisWidget.input.value = thisWidget.value; //thisWidget.input.value = newValue??? po co te 2 linie kodu?
+  isValid(value){
+   return !isNaN (value)
+   && value >= settings.amountWidget.defaultMin &&
+      value <= settings.amountWidget.defaultMax;
+     
   }
+
+  renderValue(){
+    const thisWidget = this;
+
+    thisWidget.dom.input.value = thisWidget.value;
+  }
+
   initActions() {
     const thisWidget = this;
 
-    thisWidget.input.addEventListener('change', function () {
-      thisWidget.setValue(thisWidget.input.value);
+    thisWidget.dom.input.addEventListener('change', function () {
+      thisWidget.setValue(thisWidget.dom.input.value);
     });
-    thisWidget.linkDecrease.addEventListener('click', function () {
-      thisWidget.setValue(thisWidget.value - 1); // czemu nie thisWidget.input.value??
+    thisWidget.dom.linkDecrease.addEventListener('click', function () {
+      thisWidget.setValue(thisWidget.value - 1); // czemu nie thisWidget.dom.input.value??
     });
-    thisWidget.linkIncrease.addEventListener('click', function () {
+    thisWidget.dom.linkIncrease.addEventListener('click', function () {
       thisWidget.setValue(thisWidget.value + 1);
     });
   }
-  annonce() {
-    //custom event
-    const thisWidget = this;
-
-    const event = new CustomEvent('updated', { bubbles: true });
-    thisWidget.element.dispatchEvent(event);
-  }
+  
 }
 
 export default AmountWidget;
